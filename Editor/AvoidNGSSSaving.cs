539e17d5c802d98f7dafe63a7910c6a69574aa98
JaceDaDorito <66852270+JaceDaDorito@users.noreply.github.com>
2023-07-26 16:24:08 -0400

    AvoidNGSSSaving

---
+++ Editor/AvoidNGSSSaving.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LOP.Editor
+{
+    public class AvoidNGSSSaving : UnityEditor.AssetModificationProcessor
+    {
+        public static string[] OnWillSaveAssets(string[] paths)
+        {
+            Scene scene = EditorSceneManager.GetActiveScene();
+            foreach (var gameObject in scene.GetRootGameObjects())
+            {
+                NGSS_Directional[] directionals = gameObject.GetComponentsInChildren<NGSS_Directional>();
+                foreach (NGSS_Directional dir in directionals)
+                {
+                    if (dir.NGSS_NOISE_TEXTURE)
+                        dir.NGSS_NOISE_TEXTURE.hideFlags |= HideFlags.DontSaveInEditor | HideFlags.HideAndDontSave | HideFlags.DontSaveInBuild;
+                }
+            }
+            return paths;
+        }
+    }
+}
