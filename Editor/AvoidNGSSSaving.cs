54f220cb9c5e23c52e46467dbadc6e230879e2cb
JaceDaDorito <66852270+JaceDaDorito@users.noreply.github.com>
2023-07-26 17:11:33 -0400

    hopefully this works

--- Editor/AvoidNGSSSaving.cs
+++ Editor/AvoidNGSSSaving.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using RoR2;
 
 namespace LOP.Editor
 {
@@ -8,14 +9,16 @@ namespace LOP.Editor
     {
         public static string[] OnWillSaveAssets(string[] paths)
         {
+            var addressableTexture = LegacyResourcesAPI.Load<Texture2D>("BlueNoise_R8_8");
+
             Scene scene = EditorSceneManager.GetActiveScene();
             foreach (var gameObject in scene.GetRootGameObjects())
             {
                 NGSS_Directional[] directionals = gameObject.GetComponentsInChildren<NGSS_Directional>();
                 foreach (NGSS_Directional dir in directionals)
                 {
-                    if (dir.NGSS_NOISE_TEXTURE)
-                        dir.NGSS_NOISE_TEXTURE.hideFlags |= HideFlags.DontSaveInEditor | HideFlags.HideAndDontSave | HideFlags.DontSaveInBuild;
+                    if (dir.NGSS_NOISE_TEXTURE && dir.NGSS_NOISE_TEXTURE == addressableTexture)
+                        dir.NGSS_NOISE_TEXTURE = null;
                 }
             }
             return paths;
