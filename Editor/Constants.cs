22e74e53e34aa97fdccd8b073699936c9b53b8cc
JaceDaDorito <66852270+JaceDaDorito@users.noreply.github.com>
2023-08-27 16:37:14 -0400

    Geyser Menu Item

---
+++ Editor/Constants.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace LOP
+{
+    public static class Constants
+    {
+        public static class AssetGUIDS
+        {
+            public const string geyserPrefabGUID = "be993fbf98b5dbc4198694798de33d7c";
+
+            /// <summary>
+            /// Loads an asset of type <typeparamref name="T"/> by using the <paramref name="guid"/> provided
+            /// </summary>
+            /// <typeparam name="T">The type of asset to load</typeparam>
+            /// <param name="guid">The guid to use</param>
+            /// <returns>The loaded Asset</returns>
+            public static T QuickLoad<T>(string guid) where T : Object
+            {
+                return AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(guid));
+            }
+            /// <summary>
+            /// Transforms <paramref name="guid"/> into its asset path.
+            /// </summary>
+            /// <param name="guid">The asset's guid</param>
+            /// <returns>The Asset's path</returns>
+            public static string GetPath(string guid)
+            {
+                return AssetDatabase.GUIDToAssetPath(guid);
+            }
+        }
+    }
+}
---
+++ Editor/GameobjectCreators.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using LOP;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Path = System.IO.Path;
+
+namespace LOP.Editor
+{
+    internal static class GameobjectCreators
+    {
+        [MenuItem("GameObject/Risk Of Rain 2/Geyser", false, 10)]
+        static void CreateGeyser(MenuCommand menuCommand)
+        {
+            var prefab = Constants.AssetGUIDS.QuickLoad<GameObject>(Constants.AssetGUIDS.geyserPrefabGUID);
+            var instance = UnityEngine.Object.Instantiate(prefab);
+            instance.name = "Geyser";
+            GameObjectUtility.SetParentAndAlign(instance, menuCommand.context as GameObject);
+            Undo.RegisterCreatedObjectUndo(instance, "Create " + instance.name);
+            Selection.activeObject = instance;
+        }
+    }
+}
