0630b2677437eb6dc5c3e1e925cac3ad5acbee1d
Nebby <nebby131999@gmail.com>
2026-01-11 16:46:48 -0300

    Upgrade AddressableInjector

--- Runtime/Code/AddressableInjector.cs
+++ Runtime/Code/AddressableInjector.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using HG;
+using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 using Object = UnityEngine.Object;
 
 namespace LOP
@@ -13,29 +16,101 @@ namespace LOP
     [ExecuteAlways]
     public class AddressableInjector : MonoBehaviour
     {
+        [Serializable]
+        private struct SerializedFieldOrPropertyInfo
+        {
+            [SerializeField]
+            private readonly string fieldOrPropertyName;
+            [SerializeField]
+            private readonly SerializableSystemType fieldOrPropertyType;
+
+            private MemberInfo _cachedMemberInfo;
+            public bool TryGetMemberInfo(Type declaringType, out MemberInfo result)
+            {
+                if(_cachedMemberInfo != null)
+                {
+                    result = _cachedMemberInfo;
+                    return true;
+                }
+
+                result = null;
+                MemberTypes memberTypes = MemberTypes.Field | MemberTypes.Property;
+                BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy;
+
+                MemberInfo[] memberInfos = declaringType.GetMember(fieldOrPropertyName, memberTypes, bindingFlags);
+                foreach(var memberInfo in memberInfos)
+                {
+                    if(memberInfo.Name == fieldOrPropertyName)
+                    {
+                        Type foundTypeInfo = GetFieldOrPropertyTypeFromMemberInfo(memberInfo);
+                        Type serializedType = (Type)fieldOrPropertyType;
+                        if(foundTypeInfo.IsSubclassOf(serializedType) || foundTypeInfo == serializedType)
+                        {
+                            result = memberInfo;
+                            _cachedMemberInfo = result;
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+
+            private Type GetFieldOrPropertyTypeFromMemberInfo(MemberInfo memberInfo)
+            {
+                if(memberInfo is FieldInfo fInfo)
+                {
+                    return fInfo.FieldType;
+                }
+                else if(memberInfo is PropertyInfo pInfo)
+                {
+                    return pInfo.PropertyType;
+                }
+                return null;
+            }
+        }
+
         [Tooltip("The address used for injecting")]
+        //While this is a raw string, it'll be presented in the editor as an addressable dropdown.
         public string address;
         /// <summary>
         /// The Loaded Asset
         /// </summary>
-        public Object Asset { get => _asset; private set => _asset = value; }
-        [NonSerialized] private Object _asset;
+        public Object Asset { get; private set; }
 
         [Tooltip("The component that will be injected")]
         [SerializeField] private Component targetComponent;
-        [Tooltip("The member info that'll be injected")]
-        [SerializeField] private string targetMemberInfoName;
+        [SerializeField] private SerializedFieldOrPropertyInfo targetMemberInfo;
 
+        #region Obsolete
+        [Obsolete("Utilize targetMemberInfo instead")]
+        [SerializeField] private string targetMemberInfoName; //This has the targetComponent as the preffix, it isnt that great, so instead the new one uses the type of the field/property as the preffix.
         private MemberInfo cachedMemberInfo;
+        #endregion
 
+        private AsyncOperationHandle<Object> _operationHandle;
+
+        //Refresh assigns operation handle, OnDisable must release it if it exists
         private void OnEnable() => Refresh();
+
+        private void OnDisable()
+        {
+            if(_operationHandle.IsValid())
+            {
+                Addressables.Release(_operationHandle);
+            }
+
+#if UNITY_EDITOR
+            RemoveReferencesEditor();
+#endif
+        }
 
         /// <summary>
         /// Refreshes and re-injects the asset specified in <see cref="address"/>
         /// </summary>
         public void Refresh()
         {
-            if (string.IsNullOrWhiteSpace(address) || string.IsNullOrEmpty(address))
+            if (string.IsNullOrWhiteSpace(address))
             {
 #if DEBUG
                 string msg = $"Invalid address in {this}, address is null, empty, or white space";
@@ -53,31 +128,28 @@ namespace LOP
                 return;
             }
 
-            if (string.IsNullOrEmpty(targetMemberInfoName) || string.IsNullOrWhiteSpace(targetMemberInfoName))
+            if(!TryGetMemberInfo(targetComponent.GetType(), out var memberInfo))
             {
 #if DEBUG
-                string msg = $"{this}'s targetMemberInfoName is null, empty or white space";
-                LOPLog.Warning(msg);
-#endif
-                return;
-            }
-
-            var memberInfo = GetMemberInfo();
-            if (memberInfo == null)
-            {
-#if DEBUG
-                string msg = $"{this} failed finding the MemberInfo to target based on the name \"{targetMemberInfoName}\". Target Component: {targetComponent}";
+                string msg = $"{this} failed finding the MemberInfo within {targetComponent}, this can be because the member info name was invalid.";
                 LOPLog.Warning(msg); ;
 #endif
                 return;
             }
 
-            var _asset = UnityEngine.AddressableAssets.Addressables.LoadAssetAsync<Object>(address).WaitForCompletion();
-            if (!_asset)
+            //Release operation handle if valid, this could happen if the address changes and then the behaviour cycles enabled state. or if someone manually calls Refresh
+            if(_operationHandle.IsValid())
+            {
+                Addressables.Release(_operationHandle);
+            }
+
+            _operationHandle = Addressables.LoadAssetAsync<Object>(address);
+            var asset = _operationHandle.WaitForCompletion();
+            if (!asset)
                 return;
-            Asset = _asset;
+            Asset = asset;
 #if UNITY_EDITOR
-            Asset = Instantiate(_asset);
+            Asset = Instantiate(asset);
 #endif
             Asset.hideFlags = HideFlags.DontSaveInEditor | HideFlags.NotEditable | HideFlags.DontSaveInBuild;
 
@@ -129,8 +201,14 @@ namespace LOP
             }
         }
 
-        private MemberInfo GetMemberInfo()
+        private bool TryGetMemberInfo(Type componentType, out MemberInfo memberInfo)
         {
+            if(targetMemberInfo.TryGetMemberInfo(componentType, out memberInfo))
+            {
+                return true;
+            }
+
+            //If the targetMemberInfo did not work, proceed to use the obsolete version.
             if ((cachedMemberInfo == null || $"({cachedMemberInfo.DeclaringType.Name}) {cachedMemberInfo.Name}" != targetMemberInfoName) && targetComponent)
             {
                 cachedMemberInfo = targetComponent.GetType()
@@ -143,14 +221,21 @@ namespace LOP
                     .FirstOrDefault(m => $"({m.DeclaringType.Name}) {m.Name}" == targetMemberInfoName);
             }
 
-            return cachedMemberInfo;
+            memberInfo = cachedMemberInfo;
+            return memberInfo != null;
         }
 
 #if UNITY_EDITOR
-        private void OnDisable() => RemoveReferencesEditor();
+
         private void RemoveReferencesEditor()
         {
-            var memberInfo = GetMemberInfo();
+            if (targetComponent == null)
+                return;
+
+            if(!TryGetMemberInfo(targetComponent.GetType(), out var memberInfo))
+            {
+                return;
+            }
 
             switch (memberInfo)
             {
